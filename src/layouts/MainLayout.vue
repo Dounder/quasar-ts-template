8e4719dba0dc46613f625a360e4f44b0f3121419
dcode <dr.glasdou@gmail.com>
2022-01-19 12:20:45 -0600

    done: template v1.0.0

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,37 +1,80 @@
 <template>
   <q-layout view="hHh lpR fFf">
-
     <q-header bordered class="bg-primary text-white">
       <q-toolbar>
         <q-btn dense flat round icon="menu" @click="drawer = !drawer" />
 
-        <q-toolbar-title>
-          Title
-        </q-toolbar-title>
+        <q-toolbar-title> Title </q-toolbar-title>
       </q-toolbar>
     </q-header>
 
-    <q-drawer v-model="drawer" side="left" bordered overlay>
-      <!-- drawer content -->
+    <q-drawer
+      v-model="drawer"
+      show-if-above
+      :mini="miniState"
+      @mouseover="miniState = false"
+      @mouseout="miniState = true"
+      mini-to-overlay
+      :breakpoint="400"
+      bordered
+    >
+      <q-scroll-area class="fit">
+        <q-list padding>
+          <q-item clickable v-ripple>
+            <q-item-section avatar>
+              <q-icon name="inbox" />
+            </q-item-section>
+
+            <q-item-section> Inbox </q-item-section>
+          </q-item>
+
+          <q-item active clickable v-ripple>
+            <q-item-section avatar>
+              <q-icon name="star" />
+            </q-item-section>
+
+            <q-item-section> Star </q-item-section>
+          </q-item>
+
+          <q-item clickable v-ripple>
+            <q-item-section avatar>
+              <q-icon name="send" />
+            </q-item-section>
+
+            <q-item-section> Send </q-item-section>
+          </q-item>
+
+          <q-separator />
+
+          <q-item clickable v-ripple>
+            <q-item-section avatar>
+              <q-icon name="drafts" />
+            </q-item-section>
+
+            <q-item-section> Drafts </q-item-section>
+          </q-item>
+        </q-list>
+      </q-scroll-area>
     </q-drawer>
 
     <q-page-container>
       <router-view />
     </q-page-container>
-
   </q-layout>
 </template>
 
 <script>
-import { ref } from 'vue'
+import { ref } from 'vue';
 
 export default {
-  setup () {
-    const drawer = ref(false)
+  setup() {
+    const drawer = ref(false);
+    const miniState = ref(false);
 
     return {
-      drawer
-    }
-  }
-}
+      drawer,
+      miniState
+    };
+  },
+};
 </script>
